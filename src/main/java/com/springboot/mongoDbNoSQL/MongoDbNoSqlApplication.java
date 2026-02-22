3767d0682fab79a1715bad48c4395f22277bf6cf
Pedro Cauã <pcaua6372@gmail.com>
2026-02-22 00:58:49 -0300

    Project Created

---
+++ src/main/java/com/springboot/mongoDbNoSQL/MongoDbNoSqlApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.mongoDbNoSQL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoDbNoSqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoDbNoSqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/mongoDbNoSQL/MongoDbNoSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.mongoDbNoSQL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoDbNoSqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
