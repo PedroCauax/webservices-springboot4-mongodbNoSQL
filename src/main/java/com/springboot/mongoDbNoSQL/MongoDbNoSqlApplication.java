129fa163027a9e1a6e972f2db651046ca0988f8e
Pedro Cauã <pcaua6372@gmail.com>
2026-02-26 13:36:31 -0300

    database initialization

--- src/main/java/com/springboot/mongoDbNoSQL/MongoDbNoSqlApplication.java
+++ src/main/java/com/springboot/mongoDbNoSQL/MongoDbNoSqlApplication.java
@@ -1,13 +1,18 @@
 package com.springboot.mongoDbNoSQL;
 
+import java.util.Map;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MongoDbNoSqlApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MongoDbNoSqlApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(MongoDbNoSqlApplication.class);
+        app.setDefaultProperties(Map.of(
+            "spring.data.mongodb.database", "workshop_mongo"
+        ));
+        app.run(args);
+    }
 }
