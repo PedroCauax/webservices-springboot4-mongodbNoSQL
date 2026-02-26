2bed688686883e884f7f5c5c8915c5538dc628fc
Pedro Cauã <pcaua6372@gmail.com>
2026-02-26 14:24:19 -0300

    create user

--- src/main/java/com/springboot/mongoDbNoSQL/services/UserService.java
+++ src/main/java/com/springboot/mongoDbNoSQL/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.springboot.mongoDbNoSQL.domain.User;
+import com.springboot.mongoDbNoSQL.dto.UserDTO;
 import com.springboot.mongoDbNoSQL.repository.UserRepository;
 import com.springboot.mongoDbNoSQL.services.exception.ObjectNotFoundException;
 
@@ -26,6 +27,11 @@ public class UserService {
 		 return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado")); 
 		} 
 	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
 	
-	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
 }
