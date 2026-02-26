3dc6c350879006af056f6ba9c221e99f46a2740e
Pedro Cauã <pcaua6372@gmail.com>
2026-02-26 15:31:24 -0300

    get post by id

---
+++ src/main/java/com/springboot/mongoDbNoSQL/services/PostService.java
@@ -0,0 +1,24 @@
+package com.springboot.mongoDbNoSQL.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.springboot.mongoDbNoSQL.domain.Post;
+import com.springboot.mongoDbNoSQL.repository.PostRepository;
+import com.springboot.mongoDbNoSQL.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+	
+}
